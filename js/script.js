02cdce8c2f4728727879a567e7df473a1757aaac
yatay1408 <70847533+yatay1408@users.noreply.github.com>
2024-10-17 14:52:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', 'NAME!' ],
+			strings: [ 'CHÚC', 'MỪNG', '20/10','CANOC', 'TRANG' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
